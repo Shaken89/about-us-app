d1398207520a74d5f42e8f658d2a90fb24b0ef96
Shaken Shynggyssuly <shaken2005kz@gmai.com>
2025-10-03 14:16:52 +0500

    done

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,24 +8,20 @@ export const routes: Routes = [];
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.html',   // у тебя app.html
-  styleUrls: ['./app.css'],    // у тебя app.css
+  templateUrl: './app.html',
+  styleUrls: ['./app.css'],
   standalone: true,
   imports: [CommonModule, FormsModule, RouterOutlet]
 })
 export class App {
-  // Interpolation
   title = 'About Our Team';
   mission = 'We build delightful software';
 
-  // Property binding
   photoUrl = 'src/app/img.jpg';
 
-  // Event binding
   likes = 0;
   showMessage = false;
 
-  // Two-way binding
   name = '';
   email = '';
   subscribed = false;
